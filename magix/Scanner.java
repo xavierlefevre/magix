73d6f5c618f77464ff705520940641bb75529ac7
xavierlefevre <xavier.lefevre@me.com>
2019-12-29 20:28:05 +0100

    Scan multi line comment

--- magix/Scanner.java
+++ magix/Scanner.java
@@ -101,6 +101,15 @@ class Scanner {
         // A comment goes until the end of the line.
         while (peek() != '\n' && !isAtEnd())
           advance();
+      } else if (match('*')) {
+        while (!(peek() == '*' && peekNext() == '/') && !isAtEnd()) {
+          if (peek() == '\n')
+            line++;
+          advance();
+        }
+        // The closing */
+        advance();
+        advance();
       } else {
         addToken(SLASH);
       }
