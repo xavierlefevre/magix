919145fb2d27e1d52bf875cb5592528291aadb71
xavierlefevre <xavier.lefevre@me.com>
2019-12-29 19:28:53 +0100

    Scans numbers

--- magix/Scanner.java
+++ magix/Scanner.java
@@ -98,9 +98,29 @@ class Scanner {
       break;
 
     default:
-      Magix.errorXav("Unexpected character", line, linePosition, "The character \"" + c + "\" is not valid.");
+      if (isDigit(c)) {
+        number();
+      } else {
+        Magix.errorXav("Unexpected character", line, linePosition, "The character \"" + c + "\" is not valid.");
+      }
       break;
     }
+  }
+
+  private void number() {
+    while (isDigit(peek()))
+      advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek()))
+        advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
   }
 
   private void string() {
@@ -139,6 +159,16 @@ class Scanner {
     if (isAtEnd())
       return '\0';
     return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length())
+      return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
   }
 
   private boolean isAtEnd() {
