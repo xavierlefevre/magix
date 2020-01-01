bb5a7cbc8d805532a160e7a05b838a2b0894317c
xavierlefevre <xavier.lefevre@me.com>
2020-01-01 15:26:22 +0100

    add expr operations to each expr type through the visitor pattern

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -26,6 +26,8 @@ public class GenerateAst {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    defineVisitor(writer, baseName, types);
+
     // The AST classes
     for (String type : types) {
       String className = type.split(":")[0].trim();
@@ -33,8 +35,23 @@ public class GenerateAst {
       defineType(writer, baseName, className, fields);
     }
 
+    // The base accept() method.
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
     writer.println("}");
     writer.close();
+  }
+
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+    }
+
+    writer.println("  }");
   }
 
   private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -50,6 +67,12 @@ public class GenerateAst {
       writer.println("      this." + name + " = " + name + ";");
     }
 
+    writer.println("    }");
+
+    // Visitor pattern.
+    writer.println();
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName + "(this);");
     writer.println("    }");
 
     // Fields.
