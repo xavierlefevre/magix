58e0c4a4a7a9c60d39285c3ac6e0474d5ddfa1c9
xavierlefevre <xavier.lefevre@me.com>
2020-01-16 23:07:59 +0100

    adds block statement

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -17,8 +17,8 @@ public class GenerateAst {
             "Grouping: Expr expression", "Literal: Object value", "Unary: Token operator, Expr right",
             "Variable : Token name"));
 
-    defineAst(outputDir, "Stmt", Arrays.asList("Expression : Expr expression", "Print      : Expr expression",
-        "Var        : Token name, Expr initializer"));
+    defineAst(outputDir, "Stmt", Arrays.asList("Block      : List<Stmt> statements", "Expression : Expr expression",
+        "Print      : Expr expression", "Var        : Token name, Expr initializer"));
   }
 
   private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
