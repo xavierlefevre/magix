946e2cb1a4c21b3675c3d5774416a08c40befb46
xavierlefevre <xavier.lefevre@me.com>
2020-01-19 20:08:36 +0100

    add for loop by desugaring it to while loop

--- magix/Parser.java
+++ magix/Parser.java
@@ -1,6 +1,7 @@
 package magix;
 
 import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 
 import static magix.TokenType.*;
@@ -42,6 +43,8 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(FOR))
+      return forStatement();
     if (match(IF))
       return ifStatement();
     if (match(PRINT))
@@ -52,6 +55,42 @@ class Parser {
       return new Stmt.Block(block());
 
     return expressionStatement();
+  }
+
+  private Stmt forStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+    Stmt initializer;
+    if (match(SEMICOLON)) {
+      initializer = null;
+    } else if (match(VAR)) {
+      initializer = varDeclaration();
+    } else {
+      initializer = expressionStatement();
+    }
+    Expr condition = null;
+    if (!check(SEMICOLON)) {
+      condition = expression();
+    }
+    consume(SEMICOLON, "Expect ';' after loop condition.");
+    Expr increment = null;
+    if (!check(RIGHT_PAREN)) {
+      increment = expression();
+    }
+    consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+    Stmt body = statement();
+
+    if (increment != null) {
+      body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+    }
+    if (condition == null)
+      condition = new Expr.Literal(true);
+    body = new Stmt.While(condition, body);
+    if (initializer != null) {
+      body = new Stmt.Block(Arrays.asList(initializer, body));
+    }
+
+    return body;
   }
 
   private Stmt ifStatement() {
