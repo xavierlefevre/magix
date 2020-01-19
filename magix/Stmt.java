c1084df031fbd0a33e30f5cc1dcf7139a0ab968e
xavierlefevre <xavier.lefevre@me.com>
2020-01-19 18:17:07 +0100

    add if stmt and logical operators

--- magix/Stmt.java
+++ magix/Stmt.java
@@ -8,6 +8,8 @@ abstract class Stmt {
 
     R visitExpressionStmt(Expression stmt);
 
+    R visitIfStmt(If stmt);
+
     R visitPrintStmt(Print stmt);
 
     R visitVarStmt(Var stmt);
@@ -35,6 +37,22 @@ abstract class Stmt {
     }
 
     final Expr expression;
+  }
+
+  static class If extends Stmt {
+    If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+      this.condition = condition;
+      this.thenBranch = thenBranch;
+      this.elseBranch = elseBranch;
+    }
+
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitIfStmt(this);
+    }
+
+    final Expr condition;
+    final Stmt thenBranch;
+    final Stmt elseBranch;
   }
 
   static class Print extends Stmt {
